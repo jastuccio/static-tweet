eb1a28798a1ff4c7c0b37ab63a7a077b8e763d01
joe <joe@astucc.io>
2017-12-22 10:37:53 -0500

    Add Avatar component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
 function Tweet() {
   return (
-    <div className="Tweet">
-      Yo
+    <div className="tweet">
+      <Avatar />
+      Tweet
     </div>
-  )
+  );
 }
 
-ReactDOM.render(<Tweet />, document.querySelector('#root'));
+function Avatar() {
+  return (
+    <img
+      src="https://www.gravatar.com/avatar/nothing"
+      className="avatar"
+      alt="avatar"
+    />
+  );
+}
+
+ReactDOM.render(<Tweet />, document.querySelector("#root"));
